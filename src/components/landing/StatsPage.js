648dd7282766bf7332f63962f7a76138a9bda338
Breno Almeida <brenoalmeida.dev@gmail.com>
2022-12-18 14:16:38 -0300

    [Chore]: informacoes extras

--- src/components/landing/StatsPage.js
+++ src/components/landing/StatsPage.js
@@ -7,28 +7,50 @@ export default function StatsPage(props) {
     return element.type === props.mostVoted;
   });
 
+  //props.mostVoted = Titulo (Romantico, Dramatico, Elegante);
+  //styleText.text = legenda do estilo
+  //styleText.list = array de itens que compoe o estilo
+
+  console.log(styleText.list);
   return (
     <div className={style.container}>
-      <h1 className={style.title}>SEU ESTILO É: <span className={style.estilo_negrito}>{props.mostVoted}</span></h1>
+      <h1 className={style.title}>
+        SEU ESTILO É:
+        <span className={style.estilo_negrito}>{props.mostVoted}</span>
+      </h1>
       <h5 className={style.text}>{styleText.text}</h5>
       <h4 className={style.alterText}>COMPÕEM ESSE ESTILO</h4>
-      {styleText.list.map((item) => {
-        return <li className={style.list}>{item}</li>;
-      })}
+      <p>{styleText.list}</p>
+      <ul>
+        {styleText.list.map((item) => {
+          return <li className={style.list}>{item}</li>;
+        })}
+      </ul>
       <h4 className={style.alterText}>
         Você indicou ter outras preferencias de influência de outros estilos na
         proporção abaixo. E isso é o que te faz única.
       </h4>
-      <button onClick={() => {window.location.reload();}}>Retornar a pagina inicial</button>
+      <button
+        onClick={() => {
+          window.location.reload();
+        }}
+      >
+        Retornar a pagina inicial
+      </button>
+
       {
         //TESTE FACEBOOK
       }
-      <a title="send to Facebook" 
-      href="http://www.facebook.com/sharer.php?s=100&p[title]=YOUR_TITLE&p[summary]=YOUR_SUMMARY&p[url]=YOUR_URL&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT"
-      target="_blank">
-      <span>
-        <img width="14" height="14" src="'icons/fb.gif" alt="Facebook" /> Facebook 
-      </span>
+
+      <a
+        title="send to Facebook"
+        href={`http://www.facebook.com/sharer.php?s=100&p[title]=YOUR_TITLE&p[summary]=Seu estilo é: ${props.mostVoted} </br> ${styleText.text} </br> ${styleText.list}YOUR_SUMMARY&p[url]=YOUR_URL&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT`}
+        target="_blank"
+      >
+        <span>
+          <img width="14" height="14" src="'icons/fb.gif" alt="Facebook" />{" "}
+          Facebook
+        </span>
       </a>
     </div>
   );
